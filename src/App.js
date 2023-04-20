efdaf2e23a728c19151f4f92cb6456c7e61a1d9b
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-04-20 11:30:19 +0900

    todos app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let [todos, setTodos] = useState("");
+  let [todoList, setTodoList] = useState([]);
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    // 새로고침 방지!
+    console.log(todos);
+    setTodoList([...todoList, todos]);
+  };
+
+  let handleChange = (e) => {
+    const { name, value } = e.target;
+    setTodos({ ...todos, [name]: value });
+    console.log(todos);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" id="name" onChange={handleChange} />
+        <button>ADD</button>
+      </form>
+
+      <ul>
+        {todoList.map((v) => (
+          <li> {v.name} </li>
+        ))}
+      </ul>
     </div>
   );
 }
